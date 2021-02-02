ca4adf8803f952fefd856ab46ab2a2cbd7fbb2a6
Yitzchak Horowitz <yitzchakhorowitz321@gmail.com>
2021-02-02 14:44:51 +0200

    Add GetProducts
    Add GetProducts method (GetProducts.Do)
    in the Application layer.
    Call GetProducts.Do in UI layer
    to display list of products.

--- Shop.Application/Products/GetProducts.cs
+++ Shop.Application/Products/GetProducts.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Shop.Database;
 
-namespace Shop.Application.Products
+namespace Shop.Application.GetProducts
 {
     public class GetProducts
     {
+        private ApplicationDbContext _ctx;
+
+        public GetProducts(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<ProductViewModel> Do() =>
+            _ctx.Products.ToList().Select(x => new ProductViewModel
+            {
+                Name = x.Name,
+                Description = x.Description,
+                Value = $"${ x.Value.ToString("N2")}" // 1,100.50  => 1,100.50 => $ 1,100.50
+            });
+
     }
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Value { get; set; }
+    }
+
 }
--- Shop.UI/Pages/Index.cshtml.cs
+++ Shop.UI/Pages/Index.cshtml.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Shop.Application.Products;
+
+using Shop.Application.CreateProducts;
+using Shop.Application.GetProducts;
 using Shop.Database;
 
 namespace Shop.UI.Pages
 {
     public class IndexModel : PageModel
     {
-        private  ApplicationDbContext _ctx;
+        private ApplicationDbContext _ctx;
 
         public IndexModel(ApplicationDbContext ctx)
         {
@@ -19,11 +22,12 @@ namespace Shop.UI.Pages
         }
 
         [BindProperty]
-        public ProductViewModel Product { get; set; }
+        public Application.CreateProducts.ProductViewModel Product { get; set; }
 
+        public IEnumerable< Application.GetProducts.ProductViewModel> Products { get; set; }
         public void OnGet()
         {
-
+            Products = new GetProducts(_ctx).Do();
         }
 
         public async Task<IActionResult> OnPost()
