537d6f8c70f9e4e4928061d38396afc17640894a
Yitzchak Horowitz <yitzchakhorowitz321@gmail.com>
2021-02-20 15:17:53 +0200

    Stock (Application layer)

--- Shop.Domain/Models/Stock.cs
+++ Shop.Domain/Models/Stock.cs
@@ -11,6 +11,7 @@ namespace Shop.Domain.Models
         public int Id { get; set; }
         public string Description { get; set; }
         public int Qty { get; set; }
+
         public int ProductId { get; set; }
         public Product Product { get; set; }
     }
