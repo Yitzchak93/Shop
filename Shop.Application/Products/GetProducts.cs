ff7bb66fa8cb25eb5bdd6ad99499bb8965c619fb
Yitzchak93 <yitzchakhorowitz321@gmail.com>
2021-03-02 19:57:30 +0200

    Displaying Products

--- Shop.Application/Products/GetProducts.cs
+++ Shop.Application/Products/GetProducts.cs
@@ -15,22 +15,21 @@ namespace Shop.Application.Products
         {
             _ctx = ctx;
         }
+
         public IEnumerable<ProductViewModel> Do() =>
             _ctx.Products.ToList().Select(x => new ProductViewModel
             {
-                Id = x.Id,
                 Name = x.Name,
                 Description = x.Description,
-                Value = x.Value
+                Value = $"$ {x.Value.ToString("N2")}",
             });
+
         public class ProductViewModel
         {
             public int Id { get; set; }
             public string Name { get; set; }
             public string Description { get; set; }
-            public decimal Value { get; set; }
+            public string Value { get; set; }
         }
-
     }
-
 }
